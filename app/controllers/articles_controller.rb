b0f3173a57f30e8f1809ccdda17f94629891c074
Supakorn Laohasongkram <laohasongkram.supakorn@gmail.com>
2014-10-28 11:27:07 -0700

    broken

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,16 +1,30 @@
 class ArticlesController < ApplicationController
-	def new
+	# ISNECUD
+	def index
+	  @articles = Article.all
 	end
 
-	def create
-	  @article = Article.new(article_params)
-	 
-	  @article.save
-	  redirect_to @article
+	def show
+	  @article = Article.find(params[:id])
 	end
-	 
+
+	def new
+		puts "hi"
+	  @article = Article.new
+	end
+	 
+	def create
+	  @article = Article.new(article_params)
+	 
+	  if @article.save
+	    redirect_to @article
+	  else
+	    render 'new'
+	  end
+	end
+	 
 	private
-	  def article_params
-	    params.require(:article).permit(:title, :text)
-	  end
+	  def article_params
+	    params.require(:article).permit(:title, :text)
+	  end
 end
